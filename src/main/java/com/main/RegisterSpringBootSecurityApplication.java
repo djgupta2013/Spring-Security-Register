e22b678972a582ce1e5a0e8b595d2f08cf27bc3f
djgupta2013 <djgupta2013@gmail.com>
2018-05-11 11:42:56 +0530

    first commit

---
+++ src/main/java/com/main/RegisterSpringBootSecurityApplication.java
@@ -0,0 +1,11 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterSpringBootSecurityApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterSpringBootSecurityApplication.class, args);
+	}
+}
